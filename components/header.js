159402d670eba55b18dc1ed67242db1363415333
Mahat Hussein <mahat@cloudbaron.co>
2020-12-09 11:33:00 +0000

    initial commit

--- components/header.js
+++ components/header.js
@@ -5,16 +5,16 @@ export default function Header() {
         <div class="relative flex items-center justify-between h-16">
           <div class="flex items-center px-2 lg:px-0">
             <div class="flex-shrink-0">
-              <img class="block lg:hidden h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow"/>
-              <img class="hidden lg:block h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg"alt="Workflow"/>
+              <img class="block lg:hidden h-8 w-auto" src="/mvp-stack-logo.svg" alt="MVP Stack"/>
+              <img class="hidden lg:block h-8 w-auto" src="/mvp-stack-logo.svg"alt="MVP Stack"/>
             </div>
             <div class="hidden lg:block lg:ml-6">
               <div class="flex space-x-4">
                 {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
-                <a href="#" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">Dashboard</a>
-                <a href="#" class="text-gray-900 hover:bg-gray-200 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium">Team</a>
-                <a href="#" class="text-gray-900 hover:bg-gray-200 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium">Projects</a>
-                <a href="#" class="text-gray-900 hover:bg-gray-200 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium">Calendar</a>
+                <a href="#" class="text-indigo-800 px-3 py-2 text-sm font-medium">Dashboard</a>
+                <a href="#" class="text-gray-900 hover:text-indigo-800 px-3 py-2 rounded-md text-sm font-medium">Team</a>
+                <a href="#" class="text-gray-900 hover:text-indigo-800 px-3 py-2 rounded-md text-sm font-medium">Projects</a>
+                <a href="#" class="text-gray-900 hover:text-indigo-800 px-3 py-2 rounded-md text-sm font-medium">Calendar</a>
               </div>
             </div>
           </div>
